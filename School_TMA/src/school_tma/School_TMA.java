ac48caeb30381cc4d8c5ed9c7eb71e1e90365a71
hitjava <86642555+hitjava@users.noreply.github.com>
2022-01-18 12:24:26 +0300

    Add files via upload

---
+++ School_TMA/src/school_tma/School_TMA.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+package school_tma;
+public class School_TMA 
+{   private int Floors_of_school;
+    private Stringz uni_name;
+    priv
+    
+
+     {
+            
+       
+     }
+   
+     
+     
+     
+     
+}// end of class
---
+++ print_name_id_no/src/print_name_id_no/Print_name_id_no.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package print_name_id_no;
+
+/**
+ *
+ * @author Nisar
+ */
+public class Print_name_id_no {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aisha.Nisar.Mukadam");
+        System.out.println("181027");
+        // TODO code application logic here
+    }
+    
+}
